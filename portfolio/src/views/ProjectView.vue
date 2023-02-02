cca6028b681117f88dcbe7074a0c036aeec55bf4
stegmannoah@gmail.com <stegmannoah@gmail.com>
2023-02-02 12:04:11 -0800

    vh vs height percentage changes

--- portfolio/src/views/ProjectView.vue
+++ portfolio/src/views/ProjectView.vue
@@ -4,6 +4,7 @@ import navView from '../components/nav.vue';
 import footerView from '../components/footer.vue'
 import projects from '../data/projects.json'
 
+
 </script>
 
 <template>
@@ -13,8 +14,11 @@ import projects from '../data/projects.json'
             <div class="text"><h1>Projects</h1></div>
             <div class="projectlist">
                 <div v-for="project in projects" class="project">
+                    <a :href="project.url">
                     <img class="p-img" :src="project.img"/>
                     <p>{{ project.name }}</p>
+                    <p class="info">{{ project.description }}</p>
+                    </a>
                 </div>
             </div>
         </div>
@@ -32,12 +36,19 @@ import projects from '../data/projects.json'
   width: 100%;
 }
 
+.info{
+    font-size: 20px;
+    text-align: justify;
+    padding: 20px;
+}
+
 .projectlist{
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
     flex-wrap: wrap;
+    align-items: flex-start;
     padding-bottom: 60px;
     height: fit-content;
 }
@@ -45,6 +56,7 @@ import projects from '../data/projects.json'
 h1{
     font-family: 'Open Sans', sans-serif;
     font-size: 50px;
+    margin: 2px;
     font-weight: 100;
 }
 
@@ -58,15 +70,28 @@ h1{
 .project{
     width: 330px;
     box-shadow: 0 3px 15px rgb(0 0 0 / 0.2);
-    height: 250px;
+    height: fit-content;
     margin: 25px;
+    transition: .2s all linear;
     border-radius: 25px;
     background-color: rgba(0, 0, 0, 0.2);
 }
+
+.project:hover{
+    transform: scale(1.2);
+    -webkit-transform:scale(1.2);
+    transition: .1s all linear;
+}
+
 p{
     font-family: 'Open Sans', sans-serif;
     font-size: 30px;
     margin: 0px;
+}
+
+a{
+    text-decoration: none;
+    color: black;
 }
 
 </style>
