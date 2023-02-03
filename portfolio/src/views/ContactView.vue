5878793f46099cb2655bf8cabe605a44f50570f3
stegmannoah@gmail.com <stegmannoah@gmail.com>
2023-02-02 16:14:23 -0800

    basic links

--- portfolio/src/views/ContactView.vue
+++ portfolio/src/views/ContactView.vue
@@ -7,7 +7,36 @@ import footerView from '../components/footer.vue'
 
 <template>
     <navView :notHome="true"/>
-    <div class="link-wrapper">    
+    <div class="link-wrapper">
+        <a href="../src/assets/pdf/StegmanNoahResume.pdf"><div class="resume">Resume.PDF</div></a>
+        <div class="email">stegmannoah@gmail.com</div>
     </div>
     <footerView />
 </template>
+
+
+<style scoped>
+
+.link-wrapper{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+}
+
+.link-wrapper > div, a{
+    font-family: 'IBM Plex Mono', monospace;
+    font-weight: 300;
+    font-size: calc(2vw + 10px);
+    text-decoration: none;
+}
+
+a{
+    color: rgb(39, 39, 191);
+}
+
+a:hover{
+    border-bottom: 1px solid rgb(39, 39, 191);
+}
+
+</style>
